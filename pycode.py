255e2ccc1bf98f19c4d2ef6e1625e84ae7effbf6
anonymanus817 <73923995+Ayaan817@users.noreply.github.com>
2021-06-21 13:53:44 +0530

    Create pycode.py

---
+++ pycode.py
@@ -0,0 +1,25 @@
+import random
+
+n = [88,135,192,152]
+iters = 0
+
+hits = [25,67,1,62]
+growths = [15,25,15,34]
+
+for x in n:
+    while x != 0:
+        i = random.randint(0,3)
+        iters += 1
+        if iters == 100000:
+            print("No")
+            break
+        else:
+            if x >= hits[i]:
+                x -= hits[i]
+            
+                if x == 0:
+                    print("Yes",iters)
+                    
+                else:
+                    x += growths[i]
+                    
